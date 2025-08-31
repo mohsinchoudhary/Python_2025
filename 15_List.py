a3a788b987845a8bcb35212b7a0153a9a124c0e8
mohsinchoudhary <mohsinchoudhary12@gmail.com>
2025-08-31 11:10:59 +0800

    lists

--- 15_List.py
+++ 15_List.py
@@ -2,8 +2,27 @@
 
 food = ["pizza","hamburger","hotdog","spaghetti"]
 #each item is an element
-print(food)
+#print(food)
 
 #access using indexm first position, this will print pizza
-print(food[0])
+#print(food[0])
 
+#we can always update and change elements
+food[0] = "sushi"
+#print(food[0])
+
+
+#print all elements in index using a loop
+#for x in food:
+#    print(x)
+
+
+#functions of list 
+food.append("ice cream")
+food.remove("hotdog")
+food.pop()#this will remove last element i.e ice cream
+food.insert(0,"cake")#this will add code at index 0, shushi will move to index 1
+food.sort()
+food.clear()#clear the list
+for x in food:
+    print(x)
---
+++ 16_2D_List.py
@@ -0,0 +1,13 @@
+# 2D = list of list
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert =["cake","ice cream"]
+
+food =[ drinks, dinner, dessert]
+
+print(food)
+
+#display soda
+#[][] first bracket is to access list and second bracket to access element in that list
+print(food[0][1])
