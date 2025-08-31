a8c7e1850b90bdc48164b85e7492f606f990968b
mohsinchoudhary <mohsinchoudhary12@gmail.com>
2025-08-31 11:10:37 +0800

    list

---
+++ 15_List.py
@@ -0,0 +1,9 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza","hamburger","hotdog","spaghetti"]
+#each item is an element
+print(food)
+
+#access using indexm first position, this will print pizza
+print(food[0])
+
